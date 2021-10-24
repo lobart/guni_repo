655f998cd4fb6cecbd9ff952147a74c5e3173800
lobart <lobachav@gmail.com>
2021-10-24 23:02:10 +0300

    second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import sys
 
 from wsgiref.simple_server import make_server
-from ..web.hello import application
+from web.hello import application
 httpd = make_server('0.0.0.0', 8080, application)
 httpd.serve_forever()
--- web/hello.py
+++ web/hello.py
@@ -1,21 +1,12 @@
-
-
-def application (environ, start_response):
-
+def application(environ, start_response):
     # Returns a dictionary in which the values are lists
-    d = environ['QUERY_STRING'].split("&")
-    response_body = "\n".join(d).encode('utf-8')
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
-
     # Now content type is text/plain
     response_headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(response_body)))
+        ('Content-Length', str(len(body)))
     ]
 
     start_response(status, response_headers)
-    return [response_body]
-
-
-
-# Now it is serve_forever() in instead of handle_request()
+    return iter(body)
